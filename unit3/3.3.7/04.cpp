d02098096c01950ec6402e3f4643b8c68dcde7a7
Huaqi Cui <chq_dev@163.com>
2023-05-20 22:38:21 +0800

    update

---
+++ unit3/3.3.7/04.cpp
@@ -0,0 +1,33 @@
+void dukou(string come){
+    int n=com.size();
+    stack<char> waitlist;
+    int carSum=0;
+    for(int i=0;i<n;i++){
+        if(com[i]=='k'){
+            carSum++;
+            out.push(com[i]);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        \\
+    }
+}
